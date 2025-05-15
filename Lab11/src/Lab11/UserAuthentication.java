82517d4d78302b05757c1804625c1c190a3962b8
Monis Waseem <154699440+Monis511@users.noreply.github.com>
2025-05-15 22:46:49 +0500

    Update UserAuthentication.java

--- Lab11/src/Lab11/UserAuthentication.java
+++ Lab11/src/Lab11/UserAuthentication.java
@@ -2,7 +2,7 @@ package Lab11;
 
 public class UserAuthentication {
 public void login() {
-	System.out.println("Login Authentication for User");
+	System.out.println("Login Authentication for Classmate");
 }
 }
 
